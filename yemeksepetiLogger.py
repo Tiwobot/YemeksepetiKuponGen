e5dfc66b2accad9077fa466432731ae3701a2985
tiwobot <twogun.pre@gmail.com>
2023-05-29 17:11:53 +0300

    Add Bot prevent system broker

--- yemeksepetiLogger.py
+++ yemeksepetiLogger.py
@@ -9,13 +9,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from dependencies import dependencies
+from mailgen import MailGenerator
 
 PATH = dependencies.ChromeDriverLocation
-driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.yemeksepeti.com/")
-title = driver.title
-print(title)
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+driver = webdriver.Chrome(executable_path=PATH, options=options)
+driver.execute_script(
+    "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
-driver.find_element(By.CSS_SELECTOR, ".login-label").click()
-driver.find_element(By.CSS_SELECTOR, ".button-ghost").click()
+useragentarray = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+]
+
+
+def yemeksepetiCreateAccount():
+    for i in range(len(useragentarray)):
+        driver.execute_cdp_cmd("Network.setUserAgentOverride", {
+                               "userAgent": useragentarray[i]})
+        print(driver.execute_script("return navigator.userAgent;"))
+        driver.get("https://www.yemeksepeti.com/login/new?step=email")
+        driver.find_element(By.ID, "email").click()
+        driver.find_element(By.ID, "email").send_keys(dependencies.tempMail)
