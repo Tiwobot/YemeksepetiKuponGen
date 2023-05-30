842a36c0636a9ac6fca919b31aa6f3aeb3fe66b6
tiwobot <twogun.pre@gmail.com>
2023-05-30 22:57:05 +0300

    Clean the code and add new controls

--- Scripts/data_manager.py
+++ Scripts/data_manager.py
@@ -1,16 +1,22 @@
 import pickle
 
-
-def save_mail(mail):
+def set_mailAddress(mail):
     try:
         with open("mail.data", "wb") as f:
             pickle.dump(mail, f, protocol=pickle.HIGHEST_PROTOCOL)
     except Exception as ex:
         print("Error during saving mail:", ex)
 
-def load_mail():
+def get_mailAddress():
     try:
         with open("mail.data", "rb") as f:
             return pickle.load(f)
     except Exception as ex:
-        print("Error during unpickling object (Possibly unsupported):", ex)
+        print("Error during loading mail:", ex)
+
+def get_mailLink():
+    try:
+        with open("mail.data", "rb") as f:
+            return "https://generator.email/"+pickle.load(f)
+    except Exception as ex:
+        print("Error during loading link:", ex)
--- Scripts/mail_creator.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import dependencies
-
-PATH = dependencies.ChromeDriverLocation
-driver = webdriver.Chrome(executable_path=PATH)
-driver.get("https://generator.email/")
-dependencies.tempMail = driver.find_element(By.ID, "email_ch_text").text
-driver.close()
